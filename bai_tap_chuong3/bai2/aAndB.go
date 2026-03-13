b09ce4eb63eba12504bd1b23e0ea748b147b7afc
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2026-03-13 21:30:25 +0700

    CauB - bai 2

--- bai_tap_chuong3/bai2/aAndB.go
+++ bai_tap_chuong3/bai2/aAndB.go
@@ -12,56 +12,81 @@ var (
 	resourceB sync.Mutex
 )
 
-func processA(wg *sync.WaitGroup) {
+type Result struct {
+	Success bool
+	Reason  string
+}
+
+func processA(wg *sync.WaitGroup, result chan<- Result) {
 	defer wg.Done()
+
+	res := Result{Success: false}
 
 	fmt.Println("[A] start, trying to take [resourceA]")
 	if !tryLockWithTimeout(&resourceA, 1500*time.Millisecond) {
+		res.Reason = "timeout [resourceA]"
+		result <- res
 		fmt.Println("[A] Timeout when taking [resourceA] -> skip")
 		return
 	}
+	defer resourceA.Unlock()
 	fmt.Println("[A] locked [resourceA]")
 
 	time.Sleep(400 * time.Millisecond)
 
 	fmt.Println("[A] start, trying to take [resourceB]")
 	if !tryLockWithTimeout(&resourceB, 1200*time.Millisecond) {
+		res.Reason = "timeout [resourceB]"
+		result <- res
 		fmt.Println("[A] Timeout when taking [resourceB] -> skip")
 		return
 	}
+	defer resourceB.Unlock()
 	fmt.Println("[A] locked both [resourceA] and [resourceB]")
 
+	// simulate hard work
 	time.Sleep(800 * time.Millisecond)
 
-	resourceB.Unlock()
-	resourceA.Unlock()
+	res.Success = true
+	res.Reason = "Done"
+	result <- res
 
 	fmt.Println("[A] done, unlock both")
 }
 
-func processB(wg *sync.WaitGroup) {
+func processB(wg *sync.WaitGroup, result chan<- Result) {
 	defer wg.Done()
+
+	res := Result{Success: false}
 
 	fmt.Println("[B] start, trying to take [resourceB]")
 	if !tryLockWithTimeout(&resourceB, 1500*time.Millisecond) {
+		res.Reason = "timeout [resourceB]"
+		result <- res
 		fmt.Println("[B] Timeout when taking [resourceB] -> skip")
 		return
 	}
+	defer resourceB.Unlock()
 	fmt.Println("[B] locked [resourceB]")
 
 	time.Sleep(400 * time.Millisecond)
 
 	fmt.Println("[B] start, trying to take [resourceA]")
 	if !tryLockWithTimeout(&resourceA, 1200*time.Millisecond) {
+		res.Reason = "timeout [resourceA]"
+		result <- res
 		fmt.Println("[B] Timeout when taking [resourceA] -> skip")
 		return
 	}
+	defer resourceA.Unlock()
 	fmt.Println("[B] locked both [resourceA] and [resourceB]")
 
+	// simulate hard work
 	time.Sleep(800 * time.Millisecond)
 
-	resourceA.Unlock()
-	resourceB.Unlock()
+	res.Success = true
+	res.Reason = "Done"
+	result <- res
 
 	fmt.Println("[B] done, unlock both")
 }
@@ -89,13 +114,24 @@ func main() {
 	fmt.Println("======= Deadlock + Timeout Demo ==========")
 
 	for i := 1; i <= 2; i++ {
+		resultA := make(chan Result, 1)
+		resultB := make(chan Result, 1)
 		fmt.Printf("\n-- Run %d --", i)
 		wg.Add(2)
 
-		go processA(&wg)
-		go processB(&wg)
+		go processA(&wg, resultA)
+		go processB(&wg, resultB)
 
 		wg.Wait()
+
+		ra := <-resultA
+		rb := <-resultB
+
+		if !ra.Success && !rb.Success {
+			fmt.Println("Both false, chance of DEADLOCK")
+		}
+
 		time.Sleep(300 * time.Millisecond)
+
 	}
 }
