3acb00fa70e2a729591971a0916d5c9023b12af8
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2026-03-13 23:40:23 +0700

    banker - add request alg

--- bai_tap_chuong3/bai2/cauD/main.go
+++ bai_tap_chuong3/bai2/cauD/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Banker struct {
 	Available  []int   // Size m:  How many instances of each resource type are currently free
 	Max        [][]int // Size n * m: Maximun number of each Resource type each Process my request
@@ -59,7 +61,86 @@ func (b *Banker) IsSafe() bool {
 	return true
 }
 
+// ==== Banker's Resource Request Algorithm ===
+func (b *Banker) Request(proc int, requests []int) bool {
+	// Check if the request exceeds the process’s maximum need: If Request[i] ≤ Need[i], continue; otherwise, error
+	// Check if resources are available: If Request[i] ≤ Available, continue; otherwise, the process waits.
+	for j := 0; j < len(requests); j++ {
+		if requests[j] > b.Need[proc][j] || requests[j] > b.Available[j] {
+			return false
+		}
+	}
+
+	// Temporarily allocate the resources:
+	for j := 0; j < len(requests); j++ {
+		b.Available[j] -= requests[j]
+		b.Allocation[proc][j] += requests[j]
+		b.Need[proc][j] -= requests[j]
+	}
+
+	// Run the Safety Algorithm
+	if b.IsSafe() {
+		return true
+	}
+
+	// Not safe, roll back the resources:
+	for j := 0; j < len(requests); j++ {
+		b.Available[j] += requests[j]
+		b.Allocation[proc][j] -= requests[j]
+		b.Need[proc][j] += requests[j]
+	}
+	return false
+}
+
+func NewBanker(available []int, max [][]int, allocation [][]int) *Banker {
+	/* numProcs := len(max)
+	numResType := len(max) */
+	needs := calculateNeed(max, allocation)
+
+	return &Banker{
+		Available:  available,
+		Max:        max,
+		Allocation: allocation,
+		Need:       needs,
+	}
+}
+
+// ===== Helpers =====
+func calculateNeed(max, allocation [][]int) [][]int {
+	needs := make([][]int, len(max))
+
+	for i := 0; i < len(max); i++ {
+		needs[i] = make([]int, len(max[i]))
+		for j := 0; j < len(max[i]); j++ {
+			needs[i][j] = max[i][j] - allocation[i][j]
+		}
+	}
+
+	return needs
+}
+
 func main() {
+	available := []int{3, 3, 2}
+
+	max := [][]int{
+		{7, 5, 3}, // P0
+		{3, 2, 2}, // P1
+		{9, 0, 2}, // P2
+		{2, 2, 2}, // P3
+		{4, 3, 3}, // P4
+	}
+
+	allocation := [][]int{
+		{0, 1, 0},
+		{2, 0, 0},
+		{3, 0, 2},
+		{2, 1, 1},
+		{0, 0, 2},
+	}
+
+	banker := NewBanker(available, max, allocation)
+
+	fmt.Println(banker.IsSafe())
 
 }
 
